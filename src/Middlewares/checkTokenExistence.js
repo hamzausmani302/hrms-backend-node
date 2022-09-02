95f4ed2d449bebd2b7a699b06fbfdc93e72956c3
Sheryar-Rajput <62445217+Sheryar-Rajput@users.noreply.github.com>
2022-09-02 10:17:06 +0500

    Update checkTokenExistence.js

--- src/Middlewares/checkTokenExistence.js
+++ src/Middlewares/checkTokenExistence.js
@@ -4,14 +4,18 @@ const { HTTP410Error, APIError, HTTP404Error } = require("../Utils/Error/CustomE
 const HttpStatusCode = require("../Utils/Error/HttpStatusCode");
 
 //check email in db for forget password request
-module.exports.checkTokenExistence =async  (req,res,next)=>{
-    const {email} = req.body;
-    const Token = await resetPassword.find({email :email , used: false }).catch(err=>{
-        throw new APIError("DatabaseError" , HttpStatusCode.INTERNAL_SERVER , true ,err.message)
+module.exports.checkTokenExistence = async (req, res, next) => {
+    const { email } = req.body;
+    const Token = await resetPassword.find({ email: email, used: false }).catch(err => {
+        throw new APIError("DatabaseError", HttpStatusCode.INTERNAL_SERVER, true, err.message)
     })
 
-    if(Token.length > 0 && differenceFromDate(new Date(Token[0].createdAt )) < 120 ){
-        throw new APIError("DuplicateEntry" , HttpStatusCode.INTERNAL_SERVER , true , "Duplicate token");
+    if (Token.length > 0) {
+        for (let index = 0; index < Token.length; index++) {
+            if (differenceFromDate(new Date(Token[index].createdAt)) < 120) {
+                throw new APIError("DuplicateEntry", HttpStatusCode.INTERNAL_SERVER, true, "Duplicate token");
+            }
+        }
     }
     next();
 
